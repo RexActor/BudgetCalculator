2037e7ef96ca38a7d904e28c5643bcc0d837df5b
RexActor <bobsters.lol@gmail.com>
2023-06-20 08:47:46 +0100

    Add project files.

---
+++ BudgetCalculator/Controllers/BudgetController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetCalculator.Controllers
+{
+	public class BudgetController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Edit()
+		{
+			return View();
+		}
+
+	}
+}
