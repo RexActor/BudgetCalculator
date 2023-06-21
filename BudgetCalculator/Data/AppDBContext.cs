544ee2b56700469048670f452079a1488d9fa191
RexActor <bobsters.lol@gmail.com>
2023-06-21 09:38:14 +0100

    Updated

---
+++ BudgetCalculator/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BudgetCalculator.Data
+{
+	public class AppDBContext : DbContext
+	{
+
+		public AppDBContext(DbContextOptions options) : base(options)
+		{
+
+		}
+	}
+}
